3b34ab39dd4bf8e5aa3ee3eb545299c1cee7e0c1
vvmk <vincentmasiello@gmail.com>
2018-03-19 12:46:05 -0400

    authenticate used Password.matches

--- src/main/java/user_management/security/Authenticator.java
+++ src/main/java/user_management/security/Authenticator.java
@@ -4,8 +4,7 @@ import user_management.User;
 
 public class Authenticator {
     public static boolean authenticate(User user, String passwordToCheck) {
-        Password hash = new Password(passwordToCheck);
-        return user.getPassword().equals(hash);
+        return user.getPassword().matches(passwordToCheck);
     }
 }
 
