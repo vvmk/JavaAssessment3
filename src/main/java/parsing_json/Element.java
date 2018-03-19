9f7180aeae2b802f10884befbf3bb18df7bd4d21
vvmk <vincentmasiello@gmail.com>
2018-03-19 09:34:59 -0400

    Element props/getters

--- src/main/java/parsing_json/Element.java
+++ src/main/java/parsing_json/Element.java
@@ -1,4 +1,109 @@
 package parsing_json;
 
 public class Element {
+    private String name;
+    private String appearance;
+    private Double atomicMass;
+    private Double boil;
+    private String category;
+    private String color;
+    private Double density;
+    private String discoveredBy;
+    private Double melt;
+    private Double molarHeat;
+    private String namedBy;
+    private Integer number;
+    private Integer period;
+    private String phase;
+    private String source;
+    private String spectralImage;
+    private String summary;
+    private String symbol;
+    private Integer xpos;
+    private Integer ypos;
+    private Integer[] shells;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAppearance() {
+        return appearance;
+    }
+
+    public Double getAtomicMass() {
+        return atomicMass;
+    }
+
+    public Double getBoil() {
+        return boil;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public Double getDensity() {
+        return density;
+    }
+
+    public String getDiscoveredBy() {
+        return discoveredBy;
+    }
+
+    public Double getMelt() {
+        return melt;
+    }
+
+    public Double getMolarHeat() {
+        return molarHeat;
+    }
+
+    public String getNamedBy() {
+        return namedBy;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public Integer getPeriod() {
+        return period;
+    }
+
+    public String getPhase() {
+        return phase;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public String getSpectralImage() {
+        return spectralImage;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public Integer getXpos() {
+        return xpos;
+    }
+
+    public Integer getYpos() {
+        return ypos;
+    }
+
+    public Integer[] getShells() {
+        return shells;
+    }
 }
